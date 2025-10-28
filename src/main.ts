7db1ad96c1c5d33768030f3c1ff17398c3ce874d
孙文旭 <wenxu.sun@opple.com>
2025-10-28 16:14:09 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // 设置全局前缀
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
